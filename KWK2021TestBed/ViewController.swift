95cd61c7190d7100a0f9d6c315c5d145cb2c1e6c
Matt Wecht <mattwecht@gmail.com>
2021-04-29 19:19:12 -0400

    Sample comment

--- KWK2021TestBed/ViewController.swift
+++ KWK2021TestBed/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextLable: UILabel!
+    @IBOutlet weak var userText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickMeClicked(_ sender: Any) {
+        if (userText.text == "Cats"){//sample ifelse
+            TextLable.text = "you are no longer allowed a pet"
+        }else{
+            TextLable.text = userText.text
+        }
+    }
+    
 }
 
